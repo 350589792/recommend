8846dbcbbdf9e99248344d0925c98b009e4c94b5
张振东 <1806874707@qq.com>
2020-11-30 17:07:26 +0800

    张振东修改

--- src/main/java/com/nuc/recommend/RecommendApplication.java
+++ src/main/java/com/nuc/recommend/RecommendApplication.java
@@ -1,9 +1,11 @@
 package com.nuc.recommend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.nuc.recommend.mapper")
 public class RecommendApplication {
 
     public static void main(String[] args) {
